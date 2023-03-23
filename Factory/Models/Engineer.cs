5fe8bcfa6a3bb8255e08ecd01be56646ae3c9897
Chloe ONeil <chloesporri@yahoo.com>
2023-03-23 10:14:57 -0700

    add boiler code for EngineersController with Index route

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public string Name { get; set; }
+    public int EngineerId { get; set; }
+  }
+}
